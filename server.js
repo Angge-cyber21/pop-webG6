05b8ee16bb9c885b6b045ae8930125633e8183a7
KemonitoKusuri <kacuzar1@gmail.com>
2025-05-06 10:55:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const cors = require('cors');
 app.use(cors({
   origin: 'https://angge-cyber21.github.io'
 }));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
