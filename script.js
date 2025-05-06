6dfd4b6ee70c23f0edbae9d594b6b827687fe11e
Angge-cyber21 <23-05473@g.batstate-u.edu.ph>
2025-05-06 10:57:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.getElementById('signupForm').addEventListener('submit', function(event)
     const newUsername = document.getElementById('newUsername').value;
     const newPassword = document.getElementById('newPassword').value;
 
-    fetch('https://pop-webg6.onrender.com/signup', { // Updated to use the correct signup endpoint
+    fetch('https://pop-webg6.onrender.com/signup', {// Updated to use the correct signup endpoint
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
