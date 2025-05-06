8990d28b43533a39500e61961b5688d0edc8b0f1
Angge-cyber21 <23-05473@g.batstate-u.edu.ph>
2025-05-06 09:33:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Toggle Login/Signup Views
-document.getElementById('showSignup').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.getElementById('loginContainer').style.display = 'none';
-    document.getElementById('signupContainer').style.display = 'block';
+document.getElementById("showSignup").addEventListener("click", function(e) {
+    e.preventDefault();
+    document.getElementById("loginContainer").style.display = "none";
+    document.getElementById("signupContainer").style.display = "block";
 });
 
-document.getElementById('showLogin').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.getElementById('signupContainer').style.display = 'none';
-    document.getElementById('loginContainer').style.display = 'block';
+document.getElementById("showLogin").addEventListener("click", function(e) {
+    e.preventDefault();
+    document.getElementById("signupContainer").style.display = "none";
+    document.getElementById("loginContainer").style.display = "block";
 });
 
 // Handle Login
